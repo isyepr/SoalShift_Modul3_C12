4efd953c1d989940a2977b3cf23c54f817fb0c2b
isyepr <isyep87@gmail.com>
2017-10-25 16:57:11 +0700

    aduh kenapa ini tolong

--- soal4.c
+++ soal4.c
@@ -6,21 +6,22 @@
 int in;
 pthread_t tid[111];
 
-void* faktor(void *arg){
+void* faktorial(void *arg){
 	int hsl=1;
-	for (int i=in; i>=1;i++)
+	for (int i=in; i>=1;i--)
 	hsl=hsl*i;
 	printf("Hasil %d! = %d\n", in, hsl);
 	return NULL;
 	}
-int main(int argc,char* argv){
+int main(int argc,char** argv){
 	for(int i=0; i<argc-1; i++){
 	in=0;
 	for(int j=0; j<strlen(argv[i+1]); j++){
 	in=in*10;
-	in+=argv[i+1][j]-'0');
+	in+=(argv[i+1][j]-'0');
 	}
 
-	pthread_create(&(tid[i]), NULL, &faktor, NULL);
+	pthread_create(&(tid[i]), NULL, &faktorial, NULL);
 	pthread_join(tid[i], NULL);
+}
 }
